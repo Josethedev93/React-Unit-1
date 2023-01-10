d1d0130eacff892f6a1ff8c4af87f8adc4182410
hernj07 <Hernandezj5026@gmail.com>
2023-01-09 22:04:25 -0600

    unit 1 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Square from './Square';
+
+// const handleClick = () => {
+//   setSquares(['', '', '', '', '', '', '', '', '',])
+//   setPlayer(true)
+// };
+
+function calculateWinner(array){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+      if (
+        array[a] &&
+        array[a] === array[b] &&
+        array[a] === array[c]
+    ){
+        return `${array[a]} won!`;
+    }
+  }
+  return 'Who will win?';
+};
 
 function App() {
+
+ 
+
+  const handleClick = () => {
+    setSquares(['', '', '', '', '', '', '', '', '',])
+    setPlayer(true)
+  };
+
+
+  const [squares, setSquares] = useState(['','','','','','','','','']);
+  const [player, setPlayer] = useState(true);
+  // const shape = "this is a shape"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <button onClick={handleClick}>Reset</button>
+      <div className='container'>
+        {squares.map((value, index) => {
+          return (
+            <Square
+              index={index}
+              squareValue={value}
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+            /> 
+          );
+        })}
+      </div>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 }
+
 
 export default App;
